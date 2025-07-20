99dd3c8b7f344aa6089fc08efae51e0e1b3ec7a2
tangxiangong <tangxiangong@gmail.com>
2025-07-20 16:18:13 +0800

    refactor: remove unused tailwind css asset and link

--- src/lib.rs
+++ src/lib.rs
@@ -6,14 +6,12 @@ pub mod components;
 pub mod route;
 pub mod views;
 pub static MAIN_CSS: Asset = asset!("/assets/styling/main.css");
-pub static TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
 pub static LOGO: Asset = asset!("/assets/transparent_logo.png");
 
 #[component]
 pub fn App() -> Element {
     rsx! {
         document::Link { rel: "stylesheet", href: MAIN_CSS }
-        document::Link { rel: "stylesheet", href: TAILWIND_CSS }
 
         Router::<route::Route> {}
     }
