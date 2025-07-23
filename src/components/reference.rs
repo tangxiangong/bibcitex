68d9852d1999edf35fbed6daef45416c088a75e1
tangxiangong <tangxiangong@gmail.com>
2025-07-23 09:00:31 +0800

    feat: add copy to clipboard functionality for cite key
    - Add a button to copy the citation key to the clipboard.
    
    - Display a success or failure indicator after attempting to
    copy.

--- src/components/reference.rs
+++ src/components/reference.rs
@@ -27,11 +27,47 @@ fn ChunksComp(chunks: Vec<Chunk>, cite_key: String) -> Element {
 #[component]
 pub fn Entry(entry: Reference) -> Element {
     let key = &entry.cite_key;
+    let mut copy_success = use_signal(|| true);
+    let mut copied = use_signal(|| false);
+    let copy_key = {
+        let key_clone = key.clone();
+        move |_| {
+            copied.set(true);
+            if let Ok(mut clipboard) = arboard::Clipboard::new() {
+                if clipboard.set_text(&key_clone).is_ok() {
+                    copy_success.set(true);
+                } else {
+                    copy_success.set(false);
+                }
+            } else {
+                copy_success.set(false);
+            }
+
+            spawn(async move {
+                tokio::time::sleep(tokio::time::Duration::from_millis(1500)).await;
+                copied.set(false);
+            });
+        }
+    };
 
     rsx! {
         document::Link { rel: "stylesheet", href: REFERENCE_CSS }
         div { class: "entry",
-            p { "{key}" }
+            p {
+                span { style: "color: red", "{entry.type_.to_string()} " }
+                " {key}"
+                button { onclick: copy_key,
+                    if !copied() {
+                        "📋"
+                    } else {
+                        if copy_success() {
+                            "✅"
+                        } else {
+                            "❌"
+                        }
+                    }
+                }
+            }
             if let Some(title) = &entry.title {
                 ChunksComp { chunks: title.clone(), cite_key: key.clone() }
             } else {
