0346c32a283e9bae986076f947cd4406633dc3a0
tangxiangong <tangxiangong@gmail.com>
2025-07-23 10:53:42 +0800

    feat: add hover tooltip to display reference details on mouseover

--- src/components/reference.rs
+++ src/components/reference.rs
@@ -4,6 +4,7 @@ use biblatex::Chunk;
 use dioxus::prelude::*;
 
 static REFERENCE_CSS: Asset = asset!("/assets/styling/reference.css");
+static HOVER_CSS: Asset = asset!("/assets/styling/hover.css");
 
 #[component]
 fn ChunksComp(chunks: Vec<Chunk>, cite_key: String) -> Element {
@@ -29,6 +30,17 @@ pub fn Entry(entry: Reference) -> Element {
     let key = &entry.cite_key;
     let mut copy_success = use_signal(|| true);
     let mut copied = use_signal(|| false);
+    let mut is_hovered = use_signal(|| false);
+    let mut mouse_pos = use_signal(|| (0.0, 0.0));
+    let mouseover = move |e: Event<MouseData>| {
+        is_hovered.set(true);
+        mouse_pos.set((e.client_coordinates().x, e.client_coordinates().y));
+    };
+
+    let mouseout = move |_| {
+        is_hovered.set(false);
+    };
+
     let copy_key = {
         let key_clone = key.clone();
         move |_| {
@@ -55,7 +67,12 @@ pub fn Entry(entry: Reference) -> Element {
         div { class: "entry",
             p {
                 span { style: "color: red", "{entry.type_.to_string()} " }
-                " {key}"
+                span {
+                    onmouseover: mouseover,
+                    onmouseout: mouseout,
+                    cursor: "help",
+                    " {key}"
+                }
                 button { onclick: copy_key,
                     if !copied() {
                         "📋"
@@ -72,6 +89,41 @@ pub fn Entry(entry: Reference) -> Element {
                 ChunksComp { chunks: title.clone(), cite_key: key.clone() }
             } else {
                 p { "No title found" }
+            }
+        }
+        if is_hovered() {
+            Hover { entry, mouse_x: mouse_pos().0, mouse_y: mouse_pos().1 }
+        }
+    }
+}
+
+#[component]
+pub fn Hover(entry: Reference, mouse_x: f64, mouse_y: f64) -> Element {
+    let key = &entry.cite_key;
+
+    rsx! {
+        document::Link { rel: "stylesheet", href: HOVER_CSS }
+        div {
+            class: "hover",
+            style: "left: {mouse_x + 1.0}px; top: {mouse_y + 10.0}px;",
+            p {
+                span { style: "color: red", "{entry.type_.to_string()} " }
+                " {key}"
+            }
+            p {
+                if let Some(title) = &entry.title {
+                    ChunksComp { chunks: title.clone(), cite_key: key.clone() }
+                } else {
+                    span { "No title found" }
+                }
+
+                if let Some(authors) = &entry.author {
+                    for author in authors {
+                        span { "{author}" }
+                    }
+                } else {
+                    span { "No author found" }
+                }
             }
         }
     }
