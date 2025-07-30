e60c28f3353caf8e8a5423dabdd8087e5f6b9190
tangxiangong <tangxiangong@gmail.com>
2025-07-30 18:03:05 +0800

    ui: Add spotlight shortcut button to navigation bar

--- src/views/nav.rs
+++ src/views/nav.rs
@@ -1,20 +1,39 @@
-use crate::{LOGO, route::Route};
+use crate::{LOGO, route::Route, views::open_spotlight_window};
 use dioxus::prelude::*;
 
 #[component]
 pub fn NavBar() -> Element {
+    let cmd_ctrl = {
+        #[cfg(target_os = "macos")]
+        {
+            "⌘"
+        }
+        #[cfg(not(target_os = "macos"))]
+        {
+            "Ctrl"
+        }
+    };
+    let helper = move |_| {
+        open_spotlight_window();
+    };
     rsx! {
         div {
             div { class: "navbar bg-base-100 shadow-sm",
                 div { class: "navbar-start",
                     Link { to: Route::Home {},
-                        img { src: LOGO, width: "50px" }
+                        img { src: LOGO, width: "60px" }
                     }
                 }
 
                 div { class: "navbar-center", "" }
 
-                div { class: "navbar-end", "" }
+                div { class: "navbar-end p-2",
+                    button { class: "btn btn-secondary", onclick: helper,
+                        "快捷助手"
+                        kbd { class: "kbd", "{cmd_ctrl}" }
+                        kbd { class: "kbd", "K" }
+                    }
+                }
             }
             Outlet::<Route> {}
         }
