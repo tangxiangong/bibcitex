ed988ce1a59c0457b191654cf8e2eb2636229062
tangxiangong <tangxiangong@gmail.com>
2025-08-04 10:59:10 +0800

    refactor: Move index.html content inline to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
 use bibcitex_ui::App;
 
 fn main() {
-    let index_html = include_str!("../index.html").to_string();
+    let index_html = r#"
+        <!doctype html>
+        <html data-theme="nord">
+            <head>
+                <title>Dioxus app</title>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+                />
+            </head>
+
+            <body>
+                <div id="main"></div>
+            </body>
+        </html>
+"#.to_string();
     let config = dioxus::desktop::Config::new().with_custom_index(index_html);
     dioxus::LaunchBuilder::desktop()
         .with_cfg(config)
