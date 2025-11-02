d3239ccf46bfb1eda46a86cdc9814efca52c9f33
tangxiangong <tangxiangong@gmail.com>
2025-11-02 19:14:13 +0800

    Move `#[cfg(windows)]` into main and remove stub

--- build.rs
+++ build.rs
@@ -1,11 +1,10 @@
-#[cfg(windows)]
 fn main() {
-    let mut res = winresource::WindowsResource::new();
-    res.set_icon("icons/icon.ico");
-    if let Err(e) = res.compile() {
-        eprintln!("Failed to compile Windows resources: {}", e);
+    #[cfg(windows)]
+    {
+        let mut res = winresource::WindowsResource::new();
+        res.set_icon("icons/icon.ico");
+        if let Err(e) = res.compile() {
+            eprintln!("Failed to compile Windows resources: {}", e);
+        }
     }
 }
-
-#[cfg(not(windows))]
-fn main() {}
